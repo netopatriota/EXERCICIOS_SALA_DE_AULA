67fea5646b27500b0ec8cbfc4b75d2b212b0e89f
netopatriota <neto.patriota@hotmail.com>
2016-08-17 21:22:48 -0300

    Add files via upload

---
+++ login_senha.py
@@ -0,0 +1,10 @@
+login = input ("Digite o login: ")
+senha = input ("Digite a senha: ")
+
+if senha == 'soueu123':
+
+		print ('\n' "Senha correta. Seja bem-vindo!")
+else:
+		print('\n' "Senha incorreta. Tente novamente!")
+
+#print ("O Usuario : %s, Senha: %s" %(login, senha) )
---
+++ maior_idade.py
@@ -0,0 +1,11 @@
+nome = input('Digite o seu nome: ')
+#print(nome)
+#idade = input ('Digite a idade: ')
+#print(idade)
+anonascimento = int(input ('Digite o seu ano de nascimento: '))
+anoatual = 2016-anonascimento
+print(anoatual)
+if anoatual<=18:
+    print('\n' 'Você é menor de idade.')
+else:
+    print('Você é maior de idade.')
